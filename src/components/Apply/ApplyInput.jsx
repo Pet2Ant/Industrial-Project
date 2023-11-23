4cf416a8884bacdc0f6235a19f0b11ba648f0511
Pet2Ant <niotisan@gmail.com>
2023-11-23 18:03:40 +0200

    changed input styling

--- src/components/Apply/ApplyInput.jsx
+++ src/components/Apply/ApplyInput.jsx
@@ -9,10 +9,10 @@ const Input = ({ name, setName, placeholder, type, id }) => {
     setName(inputValue);
     setInputName(inputValue);
   };
-
+// 
   return (
       <input
-        className="w-full h-12 pl-4 py-1 my-2 text-sm text-yellow-200 bg-[#A19868] rounded-2xl border border-black focus:outline-none hover:-translate-y-1 hover:translate-x-1 ease-in-out duration-500 placeholder-yellow-900 break-words overflow-wrap-break-word"
+      className="flex bg-gray-300 justify-center items-center h-10 w-full h-12 pl-4 py-1 my-2 rounded-2xl border border-black focus:outline-none hover:-translate-y-1 hover:translate-x-1 text-black text-[17px] text-sm font-medium ease-linear transition-all duration-150 hover:ring-2 hover:ring-offset-2 hover:ring-rose-500 rounded-xl"
         type={type}
         name={name}
         id={id}
