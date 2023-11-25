b970da344b66e933032f6601c6bb11b8a7fa7498
Pet2Ant <niotisan@gmail.com>
2023-11-25 17:32:39 +0200

    added table to main admin page

--- src/components/MainPage/MainPageContainer.jsx
+++ src/components/MainPage/MainPageContainer.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
+import Table from "../Applications/ApplicationTable/ApplicationsTable";
+import dataJSON from "../Applications/ApplicationTable/mockData.json";
+import { Link } from "react-router-dom";
 
 const Paragraph = ({ text }) => {
   return (
@@ -9,24 +12,71 @@ const Paragraph = ({ text }) => {
 };
 
 const RecentFeedContainer = ({ text }) => {
+  const clickHere = (
+    <>
+      Here you can view a summary of each user that has submitted an application. To view more, click{" "}
+      <Link to="/Applications" className="text-[#8D93D9] hover:text-[#143727] cursor-pointer">
+        here
+      </Link>
+      .
+    </>
+  );
+  const [headerText, setHeaderText] = useState("Applications");
+  const [subHeaderText, setSubHeaderText] = useState(clickHere);
+
+
+
+  // Table columns
+  const columns = [
+    {
+      name: "ID",
+      selector: (row) => row.id,
+      sortable: true,
+    },
+    {
+      name: "Name",
+      selector: (row) => row.first_name + " " + row.last_name,
+      sortable: true,
+    },
+    {
+      name: "Pronouns",
+      selector: (row) => row.pronouns,
+      sortable: true,
+    },
+    {
+      name: "Email",
+      selector: (row) => row.email,
+      sortable: true,
+    },
+  ];
+
+  const data = dataJSON;
   return (
-    <div className="flex flex-col justify-center items-center">
-      <p className="text-center text-2xl text-[#143727] font-medium md:leading-loose leading-relaxed">
-        {text}
-      </p>
-      <p className="text-center text-2xl text-[#143727] font-medium md:leading-loose leading-relaxed">
-        {text}
-      </p>
+    <div className="items-center ">
+      <Table
+        header={headerText}
+        subHeader={subHeaderText}
+        columns={columns}
+        data={data}
+        pagination
+      />
     </div>
   );
 };
 
 const MainPageContainer = ({ title, rows }) => {
   return (
-    <div className="bg-[#e5e5e5] h-2/3 w-3/4 scrollbar-hide overflow-hidden overflow-y-auto container flex flex-col justify-start mt-12 sm:mx-8 md:mx-12 lg:mx-24 px-8 rounded-2xl ">
+    <div className="bg-[#e5e5e5] h-2/3 w-3/4 scrollbar-hide overflow-y-auto container flex flex-col justify-start mt-12 sm:mx-8 md:mx-12 lg:mx-24 px-8 rounded-2xl ">
       <div className="flex-1">
         <h1 className="text-4xl text-center font-bold mx-auto text-[#143727] mt-4 w-full ">
-          {title}
+          {/* if title is Applications, do not show else show */}
+          {title === "Applications" ? (
+            <></>
+          ) : (
+            <>
+              {title}
+            </>
+          )}
         </h1>
         {title === "News" ? (
           <div className="my-4 ">
@@ -35,10 +85,8 @@ const MainPageContainer = ({ title, rows }) => {
             ))}
           </div>
         ) : (
-          <div className="my-4 ">
-            {rows.map((text, i) => (
-              <RecentFeedContainer key={i} text={text} />
-            ))}
+          <div className="my-4 bg-transparent">
+            <RecentFeedContainer />
           </div>
         )}
       </div>
