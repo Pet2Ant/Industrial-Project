a3a87ba75121439904842ab1ed051c986babb3d5
upgradedtofml <ckan1995@hotmail.com>
2024-02-07 00:41:52 +0200

    education handled like a normal person should have handled it

---
+++ backend/src/main/java/com/example/demo/DTO/PhDDTO.java
@@ -0,0 +1,34 @@
+package com.example.demo.DTO;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PhDDTO implements EducationDTO{
+    @JsonProperty("Education")
+    private String education;
+
+    @JsonProperty("University name: ")
+    private String universityName;
+
+    @JsonProperty("University location: ")
+    private String universityLocation;
+
+    @JsonProperty("Degree name: ")
+    private String degreeName;
+
+    @JsonProperty("Degree year: ")
+    private Integer degreeYear;
+
+    @JsonProperty("Dissertation title: ")
+    private String dissertationTitle;
+    @Override
+    public void generateDTO() {
+        if (this.education == null || this.universityName == null || this.universityLocation == null || this.degreeName == null || this.degreeYear == null || this.dissertationTitle == null) {
+            throw new IllegalArgumentException("All fields must be provided for PhDDTO");
+        }
+
+    }
+}
